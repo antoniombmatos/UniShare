c4678ed3595afdd7c057b0964ba121850384e89e
António Miguel Batista de Matos <aluno24991@ipt.pt>
2025-08-01 21:41:01 +0100

    Página de noticias e de about criada

---
+++ UniShare/Controllers/NewsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniShare.Data;
+
+namespace UniShare.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var news = await _context.News
+                .Include(n => n.Course)
+                .OrderByDescending(n => n.PublicationDate)
+                .ToListAsync();
+
+            return View(news);
+        }
+    }
+}
